57dfcdaf07a1972d40c04396738b773ec1b0b925
Harshitha B <142812352+B-Harshitha@users.noreply.github.com>
2023-11-02 20:07:52 +0530

    Add files via upload

---
+++ Alpha_or_not.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class Alpha_or_not {
+
+	public static void main(String[] args) {
+		char ch='H';
+		if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+			System.out.println("The character "+ch+" is a alphabet");
+		}
+		else
+		{
+			System.out.println("The character "+ch+" is not a alphabet");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
