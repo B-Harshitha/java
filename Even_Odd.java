785ea68f34dae2178e2966443d2da72335e1505e
Harshitha B <142812352+B-Harshitha@users.noreply.github.com>
2023-11-06 19:57:11 +0530

    Add files via upload

---
+++ Even_Odd.java
@@ -0,0 +1,19 @@
+package num;
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1;i<=10;i++) {
+			if(i%2==0) {
+				System.out.println(i+" is Even");
+			}
+			else {
+				System.out.println(i+" is Odd");
+			}
+		}
+
+	}
+
+}
