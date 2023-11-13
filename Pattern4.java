0859acaf90f55dcfce9ad67b768c7de6e14099aa
Harshitha B <142812352+B-Harshitha@users.noreply.github.com>
2023-11-13 20:13:42 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,16 @@
+package pattern;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<=4;i++) {
+			for(int j=1;j<=5-i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
